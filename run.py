069e120626aa5c0be4b822a22b42094de33a5a27
bezabu <>
2023-10-24 17:22:25 +0000

    Test ANSI escape code colours

--- run.py
+++ run.py
@@ -2,4 +2,17 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+class ANSI():
+    def background(code):
+        return "\33[{code}m".format(code=code)
+ 
+    def style_text(code):
+        return "\33[{code}m".format(code=code)
+ 
+    def color_text(code):
+        return "\33[{code}m".format(code=code)
+ 
+ 
+example_ansi = ANSI.background(97) + ANSI.color_text(96) + " TEST ANSI ESCAPE CODE"
+print(example_ansi)
 
