6d4e454981aa4edf8e75d3acd5271b7b468f9d52
bezabu <>
2023-10-24 17:34:21 +0000

    Add game object to store game data

--- run.py
+++ run.py
@@ -5,14 +5,21 @@
 class ANSI():
     def background(code):
         return "\33[{code}m".format(code=code)
- 
+
     def style_text(code):
         return "\33[{code}m".format(code=code)
- 
+
     def color_text(code):
         return "\33[{code}m".format(code=code)
- 
- 
-example_ansi = ANSI.background(97) + ANSI.color_text(96) + " TEST ANSI ESCAPE CODE"
-print(example_ansi)
 
+
+class GAME():
+    board_size = 5
+    player_name = input('Enter your name:')
+    player_score = 0
+    computer_score = 0
+
+
+example_ansi = ANSI.background(
+    97) + ANSI.color_text(96) + "HELLO "
+print(example_ansi)
