90a6e550e9f9eec768d1d659090e065687c8574b
bezabu <>
2023-10-26 15:56:58 +0000

    print_boards function now prints both player and computer boards

--- run.py
+++ run.py
@@ -26,11 +26,18 @@ class GAME():
         newlist = []
         player_board.append(newlist)
         for m in range(board_size):
-            player_board[n].append(" ") 
+            player_board[n].append("P") 
     computer_board = []
+    for n in range(board_size):
+        newlist = []
+        computer_board.append(newlist)
+        for m in range(board_size):
+            computer_board[n].append("C") 
 
     def print_boards(self):
+        print(f"      {self.player_name}'s board")
         # upper label row
+        # player
         line_print_0 = " ┌" + f"───┬"
         line_print_1 = " │" + f"   │"
         for n in range(self.board_size):
@@ -38,9 +45,18 @@ class GAME():
             line_print_1 += f" {str(self.board_label[n])} │"
         line_print_0 += "───┐"
         line_print_1 += "   │"
-        print(line_print_0)
+        # computer
+        line_print_0 += " ┌" + f"───┬"
+        line_print_1 += " │" + f"   │"
+        for n in range(self.board_size):
+            line_print_0 += f"───┬"
+            line_print_1 += f" {str(self.board_label[n])} │"
+        line_print_0 += "───┐"
+        line_print_1 += "   │"
+        print(ANSI.color_text(ANSI, 32) + line_print_0)
         print(line_print_1)
         # main body of board
+        # player
         for n in range(self.board_size):
             line_print_0 = " ├" + f"───┼"
             line_print_1 = " │" + f" {str(n+1)} │"
@@ -52,11 +68,33 @@ class GAME():
             line_print_0 += f"───┤"
             line_print_1 += f" {str(n+1)} │"
             line_print_3 += f"───┘"
+            # computer
+            line_print_0 += " ├" + f"───┼"
+            line_print_1 += " │" + f" {str(n+1)} │"
+            line_print_3 += " ├" + f"───┼"
+            for m in range(self.board_size):
+                line_print_0 += "───┼"
+                line_print_1 += " " + str(self.computer_board[0][m]) + " │"
+                line_print_3 += "───┴"
+            line_print_0 += f"───┤"
+            line_print_1 += f" {str(n+1)} │"
+            line_print_3 += f"───┘"
+            # print(line_print_0)
+            # print(line_print_1)
             print(line_print_0)
             print(line_print_1)
         # lower label row
+        # player
         line_print_0 = " ├" + f"───┼"
         line_print_1 = " │" + f"   │"
+        for n in range(self.board_size):
+            line_print_0 += f"───┼"
+            line_print_1 += f" {str(self.board_label[n])} │"
+        line_print_0 += "───┤"
+        line_print_1 += "   │"
+        # computer
+        line_print_0 += " ├" + f"───┼"
+        line_print_1 += " │" + f"   │"
         for n in range(self.board_size):
             line_print_0 += f"───┼"
             line_print_1 += f" {str(self.board_label[n])} │"
@@ -68,10 +106,14 @@ class GAME():
         for m in range(self.board_size):
             line_print_3 += "───┴"
         line_print_3 += "───┘"
+        line_print_3 += " └" + "───┴"
+        for m in range(self.board_size):
+            line_print_3 += "───┴"
+        line_print_3 += "───┘"
         print(line_print_3)
 
 
 example_ansi = ANSI.background(
     ANSI, 97) + ANSI.color_text(ANSI, 96) + "HELLO " + GAME.player_name
-print(example_ansi)
+print("\n")
 GAME.print_boards(GAME)
